afd370cf196f2cbf5b5ce4af7c572dd42ace5106
ger619 <draker74@gmail.com>
2022-12-13 15:53:16 +0300

    Add and commit on login

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_13_100156) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
+    t.string "name", default: "", null: false
+    t.string "doctor_id", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
